0b8629b62c2cbbbb8dc0d7aec003440403fc7ec6
gregory <txe16@psu.edu>
2022-12-26 13:39:46 -0500

    added vocabulary utils and tokenazation

--- Data/Data_Module.py
+++ Data/Data_Module.py
@@ -105,7 +105,9 @@ class Data_Module(pl.LightningDataModule):
         self.data_server = Data_Server(data_module=self)
         self.df = self.data_server.tokenized_dataframe
         self.vocabulary = self.data_server.vocabulary
+        self.inverse_vocabulary = self.data_server.inverse_vocabulary
         self.max_label_length = self.data_server.max_label_length
+
 
 
 
