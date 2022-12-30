d5c12a1b44bb7b37db9d6baad842069fbd0a2570
gregory <txe16@psu.edu>
2022-12-30 14:07:06 -0500

    added handwritten test modules

---
+++ Data/Handwritten_Data_Server.py
@@ -0,0 +1,154 @@
+import tarfile
+import glob
+from pathlib import Path
+import pandas as pd
+import numpy as np
+from PIL import Image
+import os
+import re
+import cv2
+from Data.configs import PrintedLatexDataConfig
+from Data.vocabulary_utils import create_vocabulary_dictionary_from_dataframe, invert_vocabulary
+import pandas as pd
+import json
+
+
+
+START_TOKEN ="<S>"
+END_TOKEN = "<E>"
+PADDING_TOKEN = "<P>"
+
+
+class Handwritten_Data_Server:
+    def __init__(self,
+                 data_module = None,
+                 ):
+
+        self.data_module = data_module
+
+        # Non-tokenized dataframe
+        self.raw_dataframe = self.get_statistics()
+
+        # tokenize and create the vocabulary
+        tokenized_dataframe_no_max_label_length = self.run_tokenizer()
+
+        # pass the max_label_length
+        self.pretokenized_dataframe = tokenized_dataframe_no_max_label_length[tokenized_dataframe_no_max_label_length['tokenized_len'] < data_module.set_max_label_length]
+        self.tokenized_dataframe = self.pretokenized_dataframe[0:data_module.number_png_images_to_use_in_dataset]
+
+        self.max_label_length =  data_module.set_max_label_length + 2 # accounting for the Start and End Tokens
+        self.vocabulary = load_dic('Data/Data_Bank/258_Test_run.json')
+
+        self.inverse_vocabulary = invert_vocabulary(self.vocabulary)
+
+
+    ########## Methods to generate Pandas DataFrame, create a vocabulary and Tokenize #########
+
+    def get_statistics(self):
+        # get dataframe
+        formulas_df = _get_dataframe()
+
+        return _get_stats(formulas_df)
+
+
+    # creates a vocabulary of tokes used for further processing
+    def run_tokenizer(self):
+        return make_vocabulary(self.raw_dataframe)
+
+
+
+
+
+####### Helper Functions for Pandas DataFrame generation ###########
+
+def _get_dataframe():
+
+    # take final formula list
+    path_to_train = PrintedLatexDataConfig.HANDWRITTEN_TRAIN
+    path_to_val = PrintedLatexDataConfig.HANDWRITTEN_VAL
+
+    # get png image names nad formula line location
+    path_to_formulas = PrintedLatexDataConfig.HANDWRITTEN_FORMULAS
+
+    formulas_df = readlines_to_df(path_to_list=path_to_val ,
+                                    path =path_to_formulas,
+                                  colname='formula',
+                                  colname_im = 'image_name')
+
+
+    return formulas_df
+
+
+# outputs formula length, image height and width.
+def _get_stats(datasetDF):
+    widths = []
+    heights = []
+    formula_lens = []
+
+    dataset = datasetDF
+    for _, row in datasetDF.iterrows():
+        image_name = row.image_name
+        # print(image_name)
+        im = Image.open(os.path.join(PrintedLatexDataConfig.HANDWRITTEN_IMAGES_FOLDER, image_name))
+        widths.append(im.size[0])
+        heights.append(im.size[1])
+        formula_lens.append(len(row.formula))
+
+    # datasetDF = datasetDF.assign(width=widths, height=heights, formula_len=formula_lens)
+    dataset['height'] = heights
+    dataset['width'] = widths
+    dataset['formula_length'] = formula_lens
+
+    return dataset
+
+
+# converts formulas txt to pandas dataframe
+def readlines_to_df(path_to_list, path, colname, colname_im):
+    rows_formulas = []
+    formula_locations = []
+    rows_images = []
+
+    n = 0
+    with open(path_to_list, 'r') as file_train_list:
+        for line in file_train_list.readlines():
+            line.strip()
+            l = line.split(' ')
+            formula_line = int(l[0])
+            image_name = l[1] + '.png'
+            rows_images.append(image_name)
+            formula_locations.append(formula_line)
+
+    # obtain the corresponding formula
+    with open(path) as file_formulas:
+        for formula_id in formula_locations:
+            file_formulas.seek(0)
+            formula = file_formulas.readlines()[formula_id]
+            formula.strip()
+            rows_formulas.append(formula)
+
+    formulas_df = pd.DataFrame({colname: rows_formulas}, dtype=np.str_)
+    images_df = pd.DataFrame({colname_im: rows_images}, dtype=np.str_)
+    formulas_df['image_name'] = images_df
+
+    return formulas_df
+
+
+def load_dic(filename):
+    with open(filename) as f:
+        dic = json.loads(f.read())
+        dic_new = dict((k, int(v)) for k, v in dic.items())
+    return dic_new
+
+
+def make_vocabulary(df_):
+
+    ## Assume that the latex formula strings are already tokenized into string-tokens separated by whitespace
+    ## Hence we just need to split the string by whitespace.
+    sr_token = df_.formula.apply(str).str.split(' ')
+
+    sr_tokenized_len = sr_token.str.len()
+    df_tokenized = df_.assign(latex_tokenized=sr_token, tokenized_len=sr_tokenized_len)
+
+
+
+    return  df_tokenized
---
+++ Data/Handwritten_tex_experiment.py
@@ -0,0 +1,204 @@
+from pathlib import Path
+import argparse
+from typing import Dict, Tuple, Collection, Union, Optional
+import torch
+import json
+from torch.utils.data import ConcatDataset, DataLoader
+import pytorch_lightning as pl
+from Data.label_transforms import Label_Transforms
+from Data.image_transforms import train_transform, test_transform, Image_Transforms
+from Data.Handwritten_Data_Server import Handwritten_Data_Server
+from Data.Base_Dataset import Base_Dataset, split_dataset
+from Data.vocabulary_utils import load_dic, invert_vocabulary
+
+
+
+
+
+
+
+'''
+
+Start, End, Pad tokens are set in vocabulary_utils.py
+
+IMAGE_HEIGHT and IMAGE_WIDTH are set in image_transforms.py (Note this will just resize the generated images)
+
+'''
+
+class Handwritten_Data_Module(pl.LightningDataModule):
+
+    def __init__(self,
+                 stage='fit',
+                 path_to_formulas = None,
+                 path_to_image_names = None,
+
+
+                 set_max_label_length=256,
+                 number_png_images_to_use_in_dataset=200*1000,
+                 labels_transform='default',
+                 image_transform_name='alb',  # or 'alb'
+
+                 load_vocabulary = False,
+                 vocabulary_path = None,
+
+                 train_val_fraction=0.9,
+
+
+                 batch_size=64,
+                 num_workers=10,
+                 data_on_gpu=False,
+
+                 ):
+
+
+        '''
+
+        :param stage:
+        :param max_label_length:
+        :param number_png_images_to_use_in_dataset:
+        :param labels_transform:
+        :param image_transform_name:
+        :param load_vocabulary:
+        :param train_test_fraction:
+        :param train_val_fraction:
+        :param augment_images:
+        :param batch_size:
+        :param num_workers:
+        :param data_on_gpu:
+
+        '''
+
+        super().__init__()
+
+        # Various input parameters
+        self.stage = stage
+        self.path_to_formulas = path_to_formulas,
+        self.path_to_image_names = path_to_image_names,
+
+        self.set_max_label_length = set_max_label_length
+        self.number_png_images_to_use_in_dataset = number_png_images_to_use_in_dataset
+        self.labels_transform = labels_transform
+
+        self.load_vocabulary = load_vocabulary
+        self.vocabulary_path = vocabulary_path
+
+        self.image_transform_name = image_transform_name
+        self.image_transform_alb = Image_Transforms.train_transform
+        self.image_transform_test = Image_Transforms.test_transform
+
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.train_val_fraction = train_val_fraction
+        self.on_gpu = data_on_gpu
+        self.shuffle_train = True
+
+
+        if load_vocabulary == True:
+            self.load_tokenizer()
+
+
+        # Data Loaders will load model-feeding data here
+        self.data_train: Union[BaseDataset, ConcatDataset]
+        self.data_val: Union[BaseDataset, ConcatDataset]
+        self.data_test: Union[BaseDataset, ConcatDataset]
+
+
+
+
+    # Uses images 'Data/generated_png_images/', formulas 'Data/final_png_formulas.txt'
+    # and image filenames 'Data/corresponding_png_images.txt'
+    # to generate a pandas tokenized dataframe
+    def prepare_data(self, *args, **kwargs):
+        self.data_server = Handwritten_Data_Server(data_module=self)
+        self.df = self.data_server.tokenized_dataframe
+        self.vocabulary = self.data_server.vocabulary
+        self.inverse_vocabulary = self.data_server.inverse_vocabulary
+        self.max_label_length = self.data_server.max_label_length
+        self.vocab_size = len(self.vocabulary)
+        self.tokenizer = Label_Transforms(vocabulary=self.vocabulary,
+                                          labels_transform_name=self.labels_transform,
+                                          max_label_length=self.max_label_length)
+
+        # funciton to turn strings into labels via a tokenizer
+        self.labels_transform_function = self.tokenizer.convert_strings_to_labels
+
+
+
+
+    def load_tokenizer(self, *args, **kwargs):
+        self.vocabulary = load_dic(self.vocabulary_path)
+        self.vocab_size = len(self.vocabulary)
+        self.inverse_vocabulary = invert_vocabulary(self.vocabulary)
+        self.max_label_length = int(self.set_max_label_length) + int(2)
+        self.tokenizer = Label_Transforms(vocabulary = self.vocabulary,
+                                          labels_transform_name = self.labels_transform,
+                                          max_label_length = int(self.max_label_length))
+
+        # funciton to turn strings into labels via a tokenizer
+        self.labels_transform_function = self.tokenizer.convert_strings_to_labels
+
+
+
+
+
+    def setup(self, stage = None):
+
+        if stage == "fit" or stage is None:
+            data_trainval = Base_Dataset(data_module = self)
+            self.data_train, self.data_val = split_dataset(base_dataset = data_trainval, fraction = self.train_val_fraction)
+            print('Train/Val Data is ready for Model loading.')
+
+        if stage == 'test':
+            self.data_test = [0]
+
+
+
+    def train_dataloader(self, *args, **kwargs) -> DataLoader:
+        """
+        construct a dataloader for training data
+        data is shuffled !
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        return DataLoader(
+            self.data_train,
+            shuffle=self.shuffle_train,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.on_gpu
+        )
+
+    def val_dataloader(self, *args, **kwargs):
+        """
+
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        return DataLoader(
+            self.data_val,
+            shuffle=False,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.on_gpu
+        )
+
+    def test_dataloader(self, *args, **kwargs):
+        """
+
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        return DataLoader(
+            self.data_test,
+            shuffle=False,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.on_gpu
+        )
+
+
+
+
