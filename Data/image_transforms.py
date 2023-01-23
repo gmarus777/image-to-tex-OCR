69137146ccf1b5510cc33a2d1963c7517110c7b2
gregory <txe16@psu.edu>
2023-01-22 20:54:23 -0500

    added collate_fn to dataloaders with padding

--- Data/image_transforms.py
+++ Data/image_transforms.py
@@ -3,10 +3,12 @@ from albumentations.pytorch import ToTensorV2
 import cv2
 
 
-
-train_transform=alb.Compose(
+# alb.augmentations.geometric.resize.Resize(height=64, width= 512, p=1),
+# albumentations.augmentations.geometric.resize.LongestMaxSize
+# albumentations.augmentations.geometric.resize.SmallestMaxSize
+train_transform_original=alb.Compose(
                     [
-
+                        alb.augmentations.geometric.resize.LongestMaxSize (max_size=1024, interpolation=1, p=1)
                         alb.Affine(scale=(0.6, 1.0), rotate=(-1, 1), cval=255, p=0.5),
                         alb.GaussNoise(var_limit=(10.0, 50.0), p=0.5),
                         alb.GaussianBlur(blur_limit=(1, 1), p=0.5),
@@ -20,9 +22,11 @@ train_transform=alb.Compose(
 
 #  alb.PadIfNeeded(min_height=128, min_width=1024, border_mode=cv2.BORDER_CONSTANT, value=255),
 
-train_transform_old =alb.Compose(
+train_transform =alb.Compose(
                     [
-
+                        alb.augmentations.geometric.resize.LongestMaxSize (max_size=1024, interpolation=1, p=1)
+                        alb.augmentations.geometric.resize.SmallestMaxSize (max_size=512, interpolation=1, p=1)
+                        alb.augmentations.geometric.resize.Resize (512, 1024, interpolation=1,  p=1)
                         alb.ShiftScaleRotate(shift_limit=0, scale_limit=(-.15, 0), rotate_limit=1, border_mode=0, interpolation=3, value=[255, 255, 255], p=1),
                         alb.Affine(scale=(0.6, 1.0), rotate=(-2, 2), cval=255, p=0.5),
                         alb.GridDistortion(distort_limit=0.2, border_mode=0, interpolation=3, value=[255, 255, 255], p=.5),
@@ -41,7 +45,9 @@ train_transform_old =alb.Compose(
 #  alb.PadIfNeeded(min_height=256, min_width=1024, border_mode=cv2.BORDER_CONSTANT, value=255),
 test_transform = alb.Compose(
     [
-
+        alb.augmentations.geometric.resize.LongestMaxSize (max_size=1024, interpolation=1, p=1)
+        alb.augmentations.geometric.resize.SmallestMaxSize (max_size=512, interpolation=1, p=1)
+        alb.augmentations.geometric.resize.Resize (512, 1024, interpolation=1,  p=1)
         # alb.Sharpen(),
         ToTensorV2(),
     ]
