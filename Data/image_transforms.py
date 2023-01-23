27d82ed5abb61663a58ef81fd35278f392cafadb
gregory <txe16@psu.edu>
2023-01-22 20:55:36 -0500

    added collate_fn to dataloaders with padding

--- Data/image_transforms.py
+++ Data/image_transforms.py
@@ -8,7 +8,7 @@ import cv2
 # albumentations.augmentations.geometric.resize.SmallestMaxSize
 train_transform_original=alb.Compose(
                     [
-                        alb.augmentations.geometric.resize.LongestMaxSize (max_size=1024, interpolation=1, p=1)
+                        alb.augmentations.geometric.resize.LongestMaxSize (max_size=1024, interpolation=1, p=1),
                         alb.Affine(scale=(0.6, 1.0), rotate=(-1, 1), cval=255, p=0.5),
                         alb.GaussNoise(var_limit=(10.0, 50.0), p=0.5),
                         alb.GaussianBlur(blur_limit=(1, 1), p=0.5),
@@ -24,9 +24,9 @@ train_transform_original=alb.Compose(
 
 train_transform =alb.Compose(
                     [
-                        alb.augmentations.geometric.resize.LongestMaxSize (max_size=1024, interpolation=1, p=1)
-                        alb.augmentations.geometric.resize.SmallestMaxSize (max_size=512, interpolation=1, p=1)
-                        alb.augmentations.geometric.resize.Resize (512, 1024, interpolation=1,  p=1)
+                        alb.augmentations.geometric.resize.LongestMaxSize (max_size=1024, interpolation=1, p=1),
+                        alb.augmentations.geometric.resize.SmallestMaxSize (max_size=512, interpolation=1, p=1),
+                        alb.augmentations.geometric.resize.Resize (512, 1024, interpolation=1,  p=1),
                         alb.ShiftScaleRotate(shift_limit=0, scale_limit=(-.15, 0), rotate_limit=1, border_mode=0, interpolation=3, value=[255, 255, 255], p=1),
                         alb.Affine(scale=(0.6, 1.0), rotate=(-2, 2), cval=255, p=0.5),
                         alb.GridDistortion(distort_limit=0.2, border_mode=0, interpolation=3, value=[255, 255, 255], p=.5),
