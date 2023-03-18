fac99735fac478272ca4824dc0edac414047c68b
gregory <txe16@psu.edu>
2023-03-18 14:26:08 -0400

    added resizing to padded image test transform

--- Data/image_transforms.py
+++ Data/image_transforms.py
@@ -64,7 +64,7 @@ class Image_Transforms:
                         alb.augmentations.geometric.resize.LongestMaxSize(max_size=256, interpolation=cv2.INTER_CUBIC,
                                                                           always_apply=True, p=1),
                         # alb.augmentations.geometric.resize.SmallestMaxSize(max_size=64, interpolation=cv2.INTER_CUBIC, always_apply=True, p=1),
-                        alb.PadIfNeeded(always_apply=True, min_height=128, min_width=512, border_mode=cv2.BORDER_CONSTANT, value=255),
+                        alb.PadIfNeeded(always_apply=True, min_height=128, min_width=256, border_mode=cv2.BORDER_CONSTANT, value=255),
 
 
                         ToTensorV2(),
@@ -88,7 +88,7 @@ class Image_Transforms:
         [ alb.augmentations.geometric.resize.LongestMaxSize (max_size=256, interpolation=cv2.INTER_CUBIC, always_apply=False, p=1),
           #alb.augmentations.geometric.resize.SmallestMaxSize(max_size=64, interpolation= cv2.INTER_CUBIC ,always_apply=False, p=1),
             #alb.augmentations.geometric.resize.Resize(interpolation= cv2.INTER_CUBIC, height=30, width= 217, p=1),
-            alb.PadIfNeeded(always_apply=True, min_height=128, min_width=512, border_mode=cv2.BORDER_CONSTANT, value=255),
+            alb.PadIfNeeded(always_apply=True, min_height=128, min_width=256, border_mode=cv2.BORDER_CONSTANT, value=255),
          alb.ToGray(always_apply=True),
          alb.Sharpen(),
          ToTensorV2(),
