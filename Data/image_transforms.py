1c74583a3886b345c03d1f9ace119b8226a721e7
gregory <txe16@psu.edu>
2023-03-18 15:05:09 -0400

    added resizing to padded image test transform

--- Data/image_transforms.py
+++ Data/image_transforms.py
@@ -85,12 +85,12 @@ class Image_Transforms:
 
     test_transform_with_padding = alb.Compose(
 
-        [ alb.augmentations.geometric.resize.LongestMaxSize (max_size=512, interpolation=cv2.INTER_CUBIC, always_apply=False, p=1),
+        [ alb.augmentations.geometric.resize.LongestMaxSize (max_size=256, interpolation=cv2.INTER_CUBIC, always_apply=False, p=1),
           #alb.augmentations.geometric.resize.SmallestMaxSize(max_size=64, interpolation= cv2.INTER_CUBIC ,always_apply=False, p=1),
             #alb.augmentations.geometric.resize.Resize(interpolation= cv2.INTER_CUBIC, height=30, width= 217, p=1),
             #alb.PadIfNeeded(always_apply=True, min_height=256, min_width=512, border_mode=cv2.BORDER_CONSTANT, value=255),
-         alb.ToGray(always_apply=True),
-         alb.Sharpen(),
+         #alb.ToGray(always_apply=True),
+         #alb.Sharpen(),
          ToTensorV2(),
          ]
     )
