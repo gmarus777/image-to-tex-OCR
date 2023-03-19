ab29beef3607ea432ef7778d60f1566619a6d3b8
gregory <txe16@psu.edu>
2023-03-19 19:46:56 -0400

    fixed image transforms

--- Data/image_transforms.py
+++ Data/image_transforms.py
@@ -54,7 +54,7 @@ class Image_Transforms:
     train_transform_with_padding = alb.Compose(
 
                     [ alb.augmentations.geometric.resize.LongestMaxSize(max_size=400, interpolation=cv2.INTER_CUBIC, always_apply=True, p=1),
-                      alb.PadIfNeeded(always_apply=True, min_height=350, min_width=400, border_mode=cv2.BORDER_CONSTANT, value=0),
+                      alb.PadIfNeeded(always_apply=True, min_height=400, min_width=400, border_mode=cv2.BORDER_CONSTANT, value=0),
 
                         alb.ShiftScaleRotate(shift_limit=0, scale_limit=(-.15, 0), rotate_limit=1, border_mode=0, interpolation=3, value=[0, 0, 0], p=1),
                         alb.Affine(scale=(0.6, 1.0), rotate=(-2, 2), cval=0, p=0.5),
@@ -90,7 +90,7 @@ class Image_Transforms:
         [ alb.augmentations.geometric.resize.LongestMaxSize (max_size=400, interpolation=cv2.INTER_CUBIC, always_apply=True, p=1),
           #alb.augmentations.geometric.resize.SmallestMaxSize(max_size=64, interpolation= cv2.INTER_CUBIC ,always_apply=False, p=1),
             #alb.augmentations.geometric.resize.Resize(interpolation= cv2.INTER_CUBIC, height=30, width= 217, p=1),
-            alb.PadIfNeeded(always_apply=True, min_height=350, min_width=400, border_mode=cv2.BORDER_CONSTANT, value=0),
+            alb.PadIfNeeded(always_apply=True, min_height=400, min_width=400, border_mode=cv2.BORDER_CONSTANT, value=0),
          alb.ToGray(always_apply=True),
          #alb.Sharpen(),
          ToTensorV2(),
