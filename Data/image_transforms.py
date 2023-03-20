c8b75e19d5365177682f81f885d89e038595fc7f
gregory <txe16@psu.edu>
2023-03-20 15:46:31 -0400

    added alb.sharepen to image transforms with padding

--- Data/image_transforms.py
+++ Data/image_transforms.py
@@ -67,7 +67,7 @@ class Image_Transforms:
                         alb.RandomBrightnessContrast(.05, (-.2, 0), True, p=0.2),
                         alb.ImageCompression(95, p=.3),
                         alb.ToGray(always_apply=True),
-
+                        alb.Sharpen(always_apply=True),
                         # alb.augmentations.geometric.resize.SmallestMaxSize(max_size=64, interpolation=cv2.INTER_CUBIC, always_apply=True, p=1),
 
 
@@ -93,7 +93,7 @@ class Image_Transforms:
          alb.RandomBrightnessContrast(.05, (-.2, 0), True, p=0.2),
          alb.ImageCompression(95, p=.3),
          alb.ToGray(always_apply=True),
-
+         alb.Sharpen(always_apply=True),
          # alb.augmentations.geometric.resize.SmallestMaxSize(max_size=64, interpolation=cv2.INTER_CUBIC, always_apply=True, p=1),
 
          ToTensorV2(),
@@ -117,28 +117,28 @@ class Image_Transforms:
         [ alb.augmentations.geometric.resize.LongestMaxSize (max_size=420, interpolation= cv2.INTER_AREA, always_apply=True, p=1),
           #alb.augmentations.geometric.resize.SmallestMaxSize(max_size=64, interpolation= cv2.INTER_CUBIC ,always_apply=False, p=1),
             #alb.augmentations.geometric.resize.Resize(interpolation= cv2.INTER_CUBIC, height=30, width= 217, p=1),
-            alb.PadIfNeeded(always_apply=True, min_height=420, min_width=420, border_mode=cv2.BORDER_CONSTANT, value=0),
+            alb.PadIfNeeded(always_apply=True, min_height=420, min_width=420, border_mode=cv2.BORDER_CONSTANT, value=1),
           alb.augmentations.crops.transforms.CenterCrop(250, 420, always_apply=True, p=1.0),
 
           #alb.Affine(scale=(0.6, 1.0), rotate=(-2, 2), cval=0, p=0.5),
+          #alb.ImageCompression(95, p=.3),
          alb.ToGray(always_apply=True),
-         # alb.Sharpen(),
+         alb.Sharpen(always_apply=True  ),
          ToTensorV2(),
          ]
     )
 
     test_transform_with_padding_small = alb.Compose(
 
-        [alb.augmentations.geometric.resize.LongestMaxSize(max_size=200, interpolation=cv2.INTER_AREA,
-                                                           always_apply=True, p=1),
+        [alb.augmentations.geometric.resize.LongestMaxSize(max_size=200, interpolation=cv2.INTER_CUBIC,always_apply=True, p=1),
          # alb.augmentations.geometric.resize.SmallestMaxSize(max_size=64, interpolation= cv2.INTER_CUBIC ,always_apply=False, p=1),
          # alb.augmentations.geometric.resize.Resize(interpolation= cv2.INTER_CUBIC, height=30, width= 217, p=1),
-         alb.PadIfNeeded(always_apply=True, min_height=420, min_width=420, border_mode=cv2.BORDER_CONSTANT, value=0),
+         alb.PadIfNeeded(always_apply=True, min_height=420, min_width=420, border_mode=cv2.BORDER_CONSTANT, value=1),
          alb.augmentations.crops.transforms.CenterCrop(250, 420, always_apply=True, p=1.0),
 
          # alb.Affine(scale=(0.6, 1.0), rotate=(-2, 2), cval=0, p=0.5),
          alb.ToGray(always_apply=True),
-         # alb.Sharpen(),
+          alb.Sharpen(),
          ToTensorV2(),
          ]
     )
