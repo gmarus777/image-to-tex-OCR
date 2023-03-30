01efe07257fa35d8766a68c53916e022eec38cf1
gregory <txe16@psu.edu>
2023-03-30 12:45:55 -0400

    chenged to white

--- Lightning_Models/Printed_Tex_Lit_Model.py
+++ Lightning_Models/Printed_Tex_Lit_Model.py
@@ -20,7 +20,7 @@ class LitResNetTransformer(pl.LightningModule):
         WandB = True,
         lr: float = 0.001,
         weight_decay: float = 0.0001,
-        milestones: List[int] = [10,20,30],
+        milestones: List[int] = [7,15,30],
         gamma: float = 0.5,
     ):
         super().__init__()
